d85c4aeee9f84ec902a5101c5d9f41fcf46bd3ee
Melody <melody.ak@hotmail.com>
2019-09-30 21:48:18 +0300

    Adds syn scan

---
+++ Ports/syn_scan.py
@@ -0,0 +1,21 @@
+from scapy.all import *
+
+dst_ip = "10.0.0.1"
+src_port = RandShort()
+dst_port=80
+
+def syn_scan(dst_ip,dst_port, scr_port):
+    print("TCP SYN scan on, %s with ports %s\n" % (dst_ip, dst_port))
+    tcp_connect_scan_resp = sr1(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=10, verbose=0)
+    if(tcp_connect_scan_resp is None):
+        return ("%s | Closed" % dst_port)
+    elif(tcp_connect_scan_resp.haslayer(TCP)):
+        if(tcp_connect_scan_resp.getlayer(TCP).flags == 0x12):
+            send_rst = sr(IP(dst=dst_ip)/TCP(sport=src_port,dport=dst_port,flags="R"),timeout=10, verbose=0)
+            return ("%s | Open" % dst_port)
+        elif(tcp_connect_scan_resp.getlayer(TCP).flags == 0x14):
+            return ("%s | Closed" % dst_port)
+    else:
+        return ("%s | Unknown response" % dst_port)
+
+print(syn_scan(dst_ip, dst_port, src_port))
