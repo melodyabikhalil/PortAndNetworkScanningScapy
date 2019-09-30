483ed65378b0f9a97d48c8afe0a20bbf4876e500
Melody <melody.ak@hotmail.com>
2019-09-30 22:15:01 +0300

    Fixes udp scan

--- Ports/udp_scan.py
+++ Ports/udp_scan.py
@@ -3,28 +3,19 @@ from scapy.all import *
 dst_ip = "10.0.0.1"
 src_port = RandShort()
 dst_port=53
-dst_timeout=10
 
-def udp_scan(dst_ip,dst_port,dst_timeout):
-    udp_scan_resp = sr1(IP(dst=dst_ip)/UDP(dport=dst_port),timeout=dst_timeout, verbose=0)
-    if (udp_scan_resp is None):
-        retrans = []
-        for count in range(0,3):
-            retrans.append(sr1(IP(dst=dst_ip)/UDP(dport=dst_port),timeout=dst_timeout, verbose=0))
-            for item in retrans:
-                if (item is None):
-                    udp_scan(dst_ip,dst_port,dst_timeout)
-                    print ("%s | Open|Filtered" % dst_port)
-                    return
-                elif (udp_scan_resp.haslayer(UDP)):
-                    print ("%s | Open" % dst_port)
-                    return
-                elif(udp_scan_resp.haslayer(ICMP)):
-                    if(int(udp_scan_resp.getlayer(ICMP).type)==3 and int(udp_scan_resp.getlayer(ICMP).code)==3):
-                        print ("%s | Closed"% dst_port)
-                        return
-                    elif(int(udp_scan_resp.getlayer(ICMP).type)==3 and int(udp_scan_resp.getlayer(ICMP).code) in [1,2,9,10,13]):
-                        print ("%s | Filtered" % dst_port)
-                        return
 
-udp_scan(dst_ip,dst_port,dst_timeout)
+def udp_scan(dst_ip,dst_port,src_port):
+    print("UDP scan on, %s with ports %s" % (dst_ip, dst_port))
+    udp_scan_resp = sr1(IP(dst=dst_ip)/UDP(sport=src_port, dport=dst_port), timeout=10, verbose=0)
+    if udp_scan_resp is None:
+        return ("%s | Open|Filtered" % dst_port)
+    elif udp_scan_resp.haslayer(ICMP):
+        return ("%s | Closed"% dst_port)
+    elif udp_scan_resp.haslayer(UDP):
+        return ("%s | Open|Filtered" % dst_port)
+    else:
+        return ("%s | Unknown response" % dst_port)
+
+
+print(udp_scan(dst_ip,dst_port,src_port))
