3aa95ea6ba664a790676bd57164519d0c7f94559
unknown <soumayard97@hotmail.com>
2019-10-15 21:09:43 +0300

    finished user interface

--- User Interface/gui.py
+++ User Interface/gui.py
@@ -33,6 +33,7 @@ def scan():
             print("Host address = "+host_address.get())
             print("Destination Port = "+dst_port.get())
             print("Source Port = "+src_port.get())
+            response_text.insert(END,"TCP SYN Scan result")
         elif(scan_mode.get() == 1):
             print("TCP Connect Scan")
             print("Host address = "+host_address.get())
@@ -70,6 +71,13 @@ def scan():
             print("Source Port = "+src_port.get())
     except:
         print('invalid host address')
+def reset():
+    host_address.delete(0, 'end')
+    scan_mode.set(0)
+    dst_port.delete(0, 'end')
+    src_port.delete(0, 'end')
+    response_text.set("")
+
 host_label = Label(port_tab, text = 'Host').grid(column = 0, row = 0, sticky = W)
 host_address = Entry(port_tab)
 host_address.grid(column = 0, row = 1, sticky = W)
@@ -93,12 +101,36 @@ src_port_label = Label(port_tab, text = 'Source Port').grid(column = 0, row = 9,
 src_port = Entry(port_tab)
 src_port.grid(column = 1, row = 9,pady = 1)
 
-scan_button = Button(port_tab, text = 'Scan', command = scan).grid(column = 0, row = 10, sticky = W, pady = 2, columnspan = 3)
-
+scan_button = Button(port_tab, text = 'Scan', command = scan).grid(column = 0, row = 10, sticky = W, pady = 2)
+reset_button = Button(port_tab,text = 'Reset', command = reset).grid(column = 1, row = 10, sticky = W, pady = 2)
+response_text = Text(port_tab, width = 35)
+response_text.grid(column = 0, row = 11,sticky = W, columnspan = 2)
 #network scanning tab
+def net_scan():
+    try:
+        address = ipaddress.ip_address(host_network_address.get())
+        #apres avoir recu le response
+        response_dummy_data = [{'ip':'172.12.12.13','mac':'rfsfsg'},{'ip':'172.12.12.13','mac':'rfsfsg'},{'ip':'172.12.12.13','mac':'rfsfsg'}]
+        for elt in response_dummy_data:
+            response_entry.insert(END,"ip "+ elt['ip'] + " " + "mac " + elt['mac'] + "\n")
+            #Label(frame,text = "ip "+ elt['ip'] + " " + "mac " + elt['mac'])
+            print( "ip "+ elt['ip'] + " " + "mac " + elt['mac'])
+    except:
+        print('invalid host address')
 host_network_label = Label(net_tab, text = 'Host').grid(column = 0, row = 0, sticky = W)
 host_network_address = Entry(net_tab)
 host_network_address.grid(column = 0, row = 1, sticky = W)
+scan_button_net = Button(net_tab, text = 'Scan', command = net_scan).grid(column = 0, row = 1)
+
+response_entry = Text(net_tab, width = 50)
+response_entry.grid(column = 0, row = 2,sticky = W)
+scrollb = Scrollbar(net_tab, command = response_entry.yview)
+response_entry['yscrollcommand'] = scrollb.set
+
+
+
+
+
 tab_control.pack(expand=1, fill='both')
-window.geometry("320x300")
+window.geometry("300x450")
 window.mainloop()
